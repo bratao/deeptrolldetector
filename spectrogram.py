fd749a22154e6e1fbc26c78222155afc69826e48
Bruno Cabral <bruno@potelo.com.br>
2018-12-06 18:37:32 -0300

    WIP

--- spectrogram.py
+++ spectrogram.py
@@ -4,6 +4,7 @@ import os
 import wave
 import pylab
 
+
 def graph_spectrogram_custom(wav_file):
     sound_info, frame_rate = get_wav_info(wav_file)
     pylab.figure(num=None, figsize=(19, 12))
@@ -12,6 +13,7 @@ def graph_spectrogram_custom(wav_file):
     pylab.specgram(sound_info, Fs=frame_rate)
     pylab.savefig('spectrogram.png')
     return sound_info
+
 
 def get_wav_info_custom(wav_file):
     wav = wave.open(wav_file, 'r')
