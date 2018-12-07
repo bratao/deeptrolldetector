f6a9087f6ac70e7e5edb7baa1acbd483ef5e5813
Bruno Cabral <bruno@potelo.com.br>
2018-12-07 11:36:59 -0300

    WIP

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-import numpy as np
-from pydub import AudioSegment
-import random
-import sys
-import io
-import os
-import glob
-import IPython
-#from spectrogram import *
-#from td_utils import *
-import sys,os,os.path
-
-
-from apiclient.discovery import build
-from httplib2 import Http
-from oauth2client import file, client, tools
+import tensorflow
